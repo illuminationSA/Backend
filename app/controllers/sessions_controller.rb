269427a51faf1bfd8cbd31f73e35b5f0c3866d55
Andres Felipe Forero Correa <afforeroc@unal.edu.co>
2017-10-21 18:22:35 -0500

    ldap auth was probed externally

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 require 'net/ldap' #gem install net-ldap
 class SessionsController < ApiController
   skip_before_action :require_login, only: [:create], raise: false
-  
+
   def connect
     ldap = Net::LDAP.new(
-      host: '192.168.99.101',
+      host: '192.168.11.5',
       port: 389,
       auth: {
              method: :simple,
@@ -14,15 +14,15 @@ class SessionsController < ApiController
     )
     return ldap.bind
   end
-  
+
   def create
     email = params[:email]
     password = params[:password]
     email = email[/\A\w+/].downcase
-    
+
     if connect()
       ldap = Net::LDAP.new(
-        host: '192.168.99.101',
+        host: '192.168.11.5',
         port: 389,
         auth: {
           method: :simple,
@@ -30,7 +30,7 @@ class SessionsController < ApiController
           password: password
         }
       )
-      if ldap.bind 
+      if ldap.bind
         puts ldap.bind
         if user = User.valid_login?(params[:email], params[:password])
           allow_token_to_be_used_only_once_for(user)
@@ -45,7 +45,7 @@ class SessionsController < ApiController
       end
     end
   end
-  
+
   #def create
   #  if user = User.valid_login?(params[:email], params[:password])
   #    allow_token_to_be_used_only_once_for(user)
